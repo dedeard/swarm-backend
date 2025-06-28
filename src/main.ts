92d58b1e2272f9289eb30e7aca6139444e639856
Dede ard <me@dedeard.my.id>
2025-06-29 00:23:03 +0800

    fix: update CORS configuration to allow all origins for broader API accessibility

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,6 @@ import { ResponseInterceptor } from './common/interceptors/response.interceptor'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  });
-
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
