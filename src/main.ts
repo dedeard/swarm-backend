e5c7fde23656bcf1b0749b16047afc952d18229b
Dede ard <me@dedeard.my.id>
2025-06-28 23:50:29 +0800

    fix: remove CORS configuration from bootstrap and add headers in Vercel routes for improved API security

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,6 @@ import { ResponseInterceptor } from './common/interceptors/response.interceptor'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: '*',
-    methods: '*',
-    allowedHeaders: '*',
-  });
-
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
